88d9243551bd4809c5c540cfa368cfe1ab70a95e
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-04-30 10:05:05 -0500

    Initial Commit

---
+++ SeaCreatures/SeaCreaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaCreaturesApp.swift
+//  SeaCreatures
+//
+//  Created by Justin Hold on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SeaCreaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
